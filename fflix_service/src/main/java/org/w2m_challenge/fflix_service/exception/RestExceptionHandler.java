8106772a45102c727ec875d20a40261f8fd84798
ManuelCode1991 <fochoa@folcode.com>
2024-11-03 20:41:39 -0300

    add exception handler

--- fflix_service/src/main/java/org/w2m_challenge/fflix_service/exception/RestExceptionHandler.java
+++ fflix_service/src/main/java/org/w2m_challenge/fflix_service/exception/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.w2m_challenge.fflix_service.exception;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private LoggerService logger;
+
+    @ExceptionHandler(value = {PropertyReferenceException.class})
+    private ResponseEntity<String> registerRestException(PropertyReferenceException e) {
+        return registerRestException(new BadRequestException(e.getMessage()));
+    }
 
     @ExceptionHandler(value = {BadRequestException.class, NotFoundException.class})
     private ResponseEntity<String> registerRestException(RestException e) {
